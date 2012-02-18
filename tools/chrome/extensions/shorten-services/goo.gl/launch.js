1e91bcb5ccb2f0a6ca71fff4859a3a24ba2951c2
Sheridan Rawlins <scr@chromium.org>
2012-02-18 10:47:46 -0800

    Added attempt to focus.

--- tools/chrome/extensions/shorten-services/goo.gl/launch.js
+++ tools/chrome/extensions/shorten-services/goo.gl/launch.js
@@ -1,4 +1,11 @@
 var shortUrl;
+
+// TODO(scr) remove this when done debugging.
+var printFocus = function() {
+    var focused = document.querySelectorAll(':focus');
+    console.log(focused);
+};
+
 var fetchData = function() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
@@ -7,6 +14,7 @@ var fetchData = function() {
             var resDiv = document.getElementById("result");
             var shortDiv = document.getElementById("short");
             var longDiv = document.getElementById("long");
+	    var shortText = document.getElementById("short-text");
 
             shortUrl = result.id;
             shortDiv.innerText = shortUrl;
@@ -14,6 +22,18 @@ var fetchData = function() {
 
             longDiv.innerText = window.intent.data;
             resDiv.style.display = "block";
+
+	    // Select the text for copying.
+	    shortText.value = shortUrl;
+	    shortText.select();
+	    shortText.focus();
+
+	    // TODO(scr) remove when done debugging.
+	    setTimeout(function timer() {
+			   printFocus();
+			   setTimeout(timer, 1000);
+		       }, 1000)
+	    printFocus();
         } 
     };
     xhr.open('POST', 'https://www.googleapis.com/urlshortener/v1/url');
@@ -21,14 +41,26 @@ var fetchData = function() {
     xhr.send(JSON.stringify({'longUrl': window.intent.data}));
 };
 
+function finishIntent() {
+    window.intent.postResult(shortUrl);
+    window.close();
+}
+
 window.addEventListener("DOMContentLoaded", function() {
 			    if(window.intent) {
 				fetchData();
 			    }
 
 			    var retButton = document.getElementById("return");
-			    retButton.addEventListener("click", function() {
-							   window.intent.postResult(shortUrl);
-							   window.close();
-						       });
+			    retButton.addEventListener("click", finishIntent);
+			    var shortForm = document.getElementById("short-form");
+			    shortForm.addEventListener('submit', finishIntent);
+
+			    // TODO(scr) remove when done debugging.
+			    var focusButton = document.getElementById("focus");
+			    focusButton.addEventListener('click', function() {
+							     var shortText = document.getElementById("short-text");
+							     shortText.select();
+							     shortText.focus();
+							 });
 			}, false);
